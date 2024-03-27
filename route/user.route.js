fb36d0e24922fdf754d7f7dd383cddc1473da339
khalid3886 <142919731+khalid3886@users.noreply.github.com>
2024-03-27 17:57:17 +0530

    a

--- route/user.route.js
+++ route/user.route.js
@@ -51,7 +51,7 @@ userRouter.post('/login',async(req,res)=>{
     }
 })
 
-userRouter.post('/logout',async(req,res)=>{
+userRouter.get('/logout',async(req,res)=>{
     const token=req.headers.authorization?.split(" ")[1]
     try{
         const logout=new LogoutModel({token})
